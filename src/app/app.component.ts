558af0b22ee64403ef4005758ef23ab33bc81c8a
Bhushan Gavhale <bhushan>
2024-08-09 11:00:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pw';
+  password = ''
+
+  onClickedButton(){
+    this.password = 'My Password'
+  }
 }
